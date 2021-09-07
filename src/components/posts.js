72171389d3eeea0995b6a2f51864a4bf1d36cbfb
Roopa Bhandari <bhandari.roopa44@gmail.com>
2021-09-07 22:20:17 +0530

    page navigation

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,43 +2,78 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css';
 
-export default class Posts extends React.Component{
-    constructor(props){
+export default class Posts extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
-            data:[],
-            first:[],
-            initial:0
+        this.state = {
+            data: [],
+            first: [],
+            initial: 0,
+            currentPage: 1,
+            totalPages: 0
         }
     }
-    componentDidMount(){
-      axios.get('https://jsonplaceholder.typicode.com/posts').then((result)=>{
-          console.log(result);
-          this.setState({
-              data:result.data,
-              first:result.data.slice(0,10)
-          })
-      }).catch((err)=>{
-        console.log(err)
-      })
-    }
-    loadItems=()=>{
-        console.log(this.state.initial)
-        const value = this.state.initial +10 
-        const newArray = [...this.state.data];
-        const first = newArray.splice(value,10);
-        console.log(first)
-        this.setState({
-            first :first,
-            initial : value
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((result) => {
+            console.log(result);
+            this.setState({
+                data: result.data,
+                totalPages: result.data.length / 10,
+                first: result.data.slice(0, 10)
+            })
+        }).catch((err) => {
+            console.log(err)
         })
     }
-    render(){
-        return(
+    loadItems = () => {
+        console.log(this.state.initial)
+        const value = this.state.initial + 10
+        const newArray = [...this.state.data];
+        const first = newArray.splice(value, 10);
+        console.log(first)
+        this.setState({
+            first: first,
+            initial: value
+        })
+    }
+
+    previousItems = () => {
+        console.log(this.state.initial)
+        const value = this.state.initial - 10
+        const pageNumber = this.state.currentPage - 1
+        const newArray = [...this.state.data];
+        const first = newArray.splice(value, 10);
+        console.log(first)
+        this.setState({
+            first: first,
+            initial: value,
+            currentPage: pageNumber,
+        })
+    }
+
+    nextItems = () => {
+        console.log(this.state.initial)
+        const value = this.state.initial + 10
+        const pageNumber = this.state.currentPage + 1
+        const newArray = [...this.state.data];
+        const first = newArray.splice(value, 10);
+        console.log(first)
+        this.setState({
+            first: first,
+            initial: value,
+            currentPage: pageNumber,
+        })
+    }
+
+
+    render() {
+        return (
             <div className="container">
                 <h4>Posts</h4>
-                <table className ="table table-bordered">
-                <caption> <button className="btn btn-primary" onClick={this.loadItems}>Next</button></caption>
+                 <p class="float-right" style={{ float :"right"}}>Page: {this.state.currentPage}/{this.state.totalPages}</p>
+                <table className="table table-bordered">
+                    <caption>
+                    </caption>
                     <thead>
                         <tr>
                             <th>Id</th>
@@ -46,17 +81,25 @@ export default class Posts extends React.Component{
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.first === undefined? "":
-                        this.state.first.map((item,i)=>(
-                            <tr key={i}>
-                            <td>{item.id}</td>
-                            <td>{item.title}</td>
-                         </tr>
-                        ))
+                        {this.state.first === undefined ? "" :
+                            this.state.first.map((item, i) => (
+                                <tr key={i}>
+                                    <td>{item.id}</td>
+                                    <td>{item.title}</td>
+                                </tr>
+                            ))
                         }
                     </tbody>
                 </table>
-                
+
+                <div class="row justify-content-center">
+                    <div class="col-3 text-center">
+                        <button className="btn btn-primary" style={{ marginRight : "10px"}} disabled={this.state.currentPage === 1 ? true : false} onClick={this.previousItems}>Previous</button>
+                        <button className="btn btn-primary" disabled={this.state.currentPage === this.state.totalPages ? true : false} onClick={this.nextItems}>Next</button>
+                        
+                    </div>
+                </div>
+
             </div>
         )
     }
